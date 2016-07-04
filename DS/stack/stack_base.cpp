67799bdd80339b36c3c769bd9fb9377182379ec3
AadityaJ <aadityajamuar@gmail.com>
2016-07-04 19:37:06 +0530

    adding

---
+++ DS/stack/stack_base.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define MAX_SIZE 1000
+class stack
+{
+	int size;
+	int arr[];
+public:
+	stack(){size=0;}
+	void push(int);
+	int pop();
+	void display;
+};
+void stack::push(int item){
+	if()
+}
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
