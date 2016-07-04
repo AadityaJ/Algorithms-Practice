8eb990fccaaed3824a6ce56fba54bcd49ae7d697
AadityaJ <aadityajamuar@gmail.com>
2016-07-04 20:00:06 +0530

    adding

--- DS/stack/stack_base.cpp
+++ DS/stack/stack_base.cpp
@@ -8,13 +8,42 @@ public:
 	stack(){size=0;}
 	void push(int);
 	int pop();
-	void display;
+	void display();
 };
 void stack::push(int item){
-	if()
+	if(size==MAX_SIZE){printf("Stack Overflow\n");return;}
+	arr[size++]=item;
+}
+int stack::pop(){
+	if(size==0){printf("Stack underflow\n");return -1;}
+	return arr[--size];
+}
+void stack::display(){
+	if(size==0){printf("Stack underflow\n");return;}
+	for(int i=0;i<size;i++){
+		printf("%d\t",arr[i]);
+	}
+	printf("\n");
 }
 int main(int argc, char const *argv[])
 {
-	
+	int menu=1;
+	int val;
+	stack st;
+	while(menu){
+		printf("Input menu :: 1.push 2.pop 3.display 0.exit\n");
+		scanf("%d",&menu);
+		switch(menu){
+			case 1: scanf("%d",&val);
+					st.push(val);
+					break;
+			case 2: printf("%d\n",st.pop());
+					break;
+			case 3: st.display();
+					break;
+			
+
+		}
+	}
 	return 0;
 }
