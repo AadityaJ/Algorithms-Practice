8bcac1684ef7014560691c62522c95e5410abf22
AadityaJ <aadityajamuar@gmail.com>
2016-07-09 19:30:01 +0530

    errors everywhere

--- DS/linkedlist/base.cpp
+++ DS/linkedlist/base.cpp
@@ -1,58 +1,50 @@
-/* double ended queue based on linked list
-*/
 #include <stdio.h>
 
-struct node{
+class node{
     int info;
     node* next;
-};
-class ll{
-    node *head;
-    node *tail;
 public:
-    ll(){head=tail=NULL;}
     void insertBeg(int);
     void insertEnd(int);
-    node* delBeg();
-    node* delEnd();
+    int delBeg();
+    int delEnd();
     void printAll();
 };
-void ll::insertBeg(int x){
+node *head=NULL;
+node *tail=NULL;
+void node::insertBeg(int x){
     node *temp=new node;
     temp->info=x;
-    if(!head &&!tail){
-        headtemp;tail=temp;return;
-    }
+    temp->next=NULL;
+    if(!head && !tail) {head=tail=temp;return;}
     temp->next=head;
     head=temp;
     delete(temp);
 }
-void ll::insertEnd(int x){
+void node::insertEnd(int x){
     node *temp=new node;
     temp->info=x;
-    if(!head && !temp){
-        head=tail=temp;return;
-    }
-    tail->next=temp;
     temp->next=NULL;
-    delete(temp);
+    if(!head && !tail) {head=tail=temp;return;}
+    tail->next=temp;
+    tail=temp;
 }
-node* ll::delBeg(){
+int node::delBeg(){
     node *temp=new node;
     if(!head &&!tail){
         // empty linked list
         printf("Empty linked list\n");
-        return head;
+        return -1;
     }
     temp=head;
     head=head->next;
-    return temp;
+    return temp->info;
 }
-node* ll::delEnd(){
+int node::delEnd(){
     if(!head &&!tail){
         // empty linked list
         printf("Empty linked list\n");
-        return 0;
+        return -1;
     }
     node *curr=head;
     while(curr->next==tail){
@@ -61,9 +53,9 @@ node* ll::delEnd(){
     node *temp=new node;
     temp=tail;
     tail=curr;
-    return temp;
+    return temp->info;
 }
-void ll::printAll(){
+void node::printAll(){
     if(!head && !tail){
         printf("Empty Linked List\n");return;
     }printf("\n");
@@ -72,23 +64,20 @@ void ll::printAll(){
     }
     printf("\n");
 }
-
-
-
 int main(int argc, char const *argv[]) {
-    ll l;
-    node *temp;
+    node n;
+    int temp;
     int menu=1;
     int x;
     while(menu){
         printf("1.insertBeg 2.insertEnd 3.delBeg 4.delEnd 5.printAll 0.exit\n");
         scanf("%d",&menu);
         switch (menu) {
-            case 1:scanf("%d",&x);l.insertBeg(x);break;
-            case 2:scanf("%d",&x);l.insertEnd(x);break;
-            case 3:temp=l.delBeg();printf("%d\n",temp->info);break;
-            case 4:temp=l.delEnd();printf("%d\n",temp->info);break;
-            case 5:l.printAll();break;
+            case 1:scanf("%d",&x);n.insertBeg(x);break;
+            case 2:scanf("%d",&x);n.insertEnd(x);break;
+            case 3:temp=n.delBeg();printf("%d\n",temp);break;
+            case 4:temp=n.delEnd();printf("%d\n",temp);break;
+            case 5:n.printAll();break;
         }
     }
     return 0;
