392dda1f37684538d31309fb8c268835f43a90f0
AadityaJ <aadityajamuar@gmail.com>
2016-07-12 20:39:29 +0530

    adding circular ll

--- DS/linkedlist/circular_ll.cpp
+++ DS/linkedlist/circular_ll.cpp
@@ -1,80 +1,115 @@
+/* circular linked list
+*/
 #include <stdio.h>
 
 struct node{
     int info;
     node* next;
 };
-class ll{
+class c_ll{
     node *head;
-    node *tail;
 public:
-    ll(){head=tail=NULL;}
+    c_ll(){head=NULL;}
     void insertBeg(int);
     void insertEnd(int);
     node* delBeg();
     node* delEnd();
     void printAll();
 };
-void ll::insertBeg(int x){
+void c_ll::insertBeg(int x){
     node *temp=new node;
     temp->info=x;
-    if(!head &&!tail){
-        head=temp;tail=temp;
-    }else{
+    if(!head){
+        head=temp;head->next=head;return;
+    }
+    if((head->next)==head){
+        head->next=temp;
+        temp->next=head;
+        head=temp;
+        return;
+    }
+    // REACH END AND CHANGE LOOP
+    node *curr=head;
+    while(curr->next!=head){curr=curr->next;}
+    curr->next=temp;
     temp->next=head;
     head=temp;
-    }
 }
-void ll::insertEnd(int x){
+void c_ll::insertEnd(int x){
     node *temp=new node;
     temp->info=x;
-    if(!head && !temp){
-        head=tail=temp;
-    }else{
-    tail->next=temp;
-    temp->next=NULL;
-  }
-}
-node* ll::delBeg(){
-    node *temp=new node;
-    if(!head &&!tail){
-        // empty linked list
-        printf("Empty linked list\n");
-        return head;
+    if(!head){
+        head=temp;head->next=head;return;
     }
-    temp=head;
-    head=head->next;
-    return temp;
-}
-node* ll::delEnd(){
-    if(!head &&!tail){
-        // empty linked list
-        printf("Empty linked list\n");
-        return head;
+    if(head->next==head){
+        head->next=temp;
+        temp->next=head;
+        return;
     }
     node *curr=head;
-    while(curr->next!=tail){
+    while(curr->next!=head){curr=curr->next;}
+    curr->next=temp;
+    temp->next=head;
+
+}
+node* c_ll::delBeg(){
+    node *temp=new node;
+    if(!head){
+        // empty linked list
+        printf("Empty linked list\n");
+        return head;
+    }
+    if(head->next==head){
+        temp=head;
+        head=NULL;
+        return temp;
+    }
+    // find end first
+    node *curr=head;
+    while(curr->next!=head){curr=curr->next;}
+    curr->next=head->next;
+    temp=head;
+    head=head->next;
+    return(temp);
+}
+node* c_ll::delEnd(){
+    node *temp=new node;
+    if(!head){
+        // empty linked list
+        printf("Empty linked list\n");
+        return head;
+    }
+    if(head->next==head){
+        temp=head;
+        head=NULL;
+        return temp;
+    }
+    node *curr=head;
+    //find last but one end first
+    while((curr->next)->next!=head){
         curr=curr->next;
     }
-    node *temp=new node;
-    temp=tail;
-    tail=curr;
+    temp=curr->next;
+    curr->next=head;
     return temp;
 }
-void ll::printAll(){
-    if(!head && !tail){
+void c_ll::printAll(){
+    if(!head){
         printf("Empty Linked List\n");return;
     }printf("\n");
-    for(node *curr=head;curr;curr=curr->next){
+    node *curr=head;
+    do{
         printf("%d\t",curr->info);
-    }
+        curr=curr->next;
+    }while(curr!=head);
+
     printf("\n");
 }
 
 
 
 int main(int argc, char const *argv[]) {
-    ll l;
+    c_ll l;
     node *temp;
     int menu=1;
     int x;
