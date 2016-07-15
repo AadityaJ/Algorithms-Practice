265eb99f3e2b54356afe420faf4a9754dbb764a1
AadityaJ <aadityajamuar@gmail.com>
2016-07-15 21:48:23 +0530

    adding levelorder

--- DS/trees/bst_base.cpp
+++ DS/trees/bst_base.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <queue>
+using namespace std;
 struct node{
     int data;
     node *lcl;
@@ -13,6 +14,7 @@ public:
     void postorder(node *);
     void preorder(node*);
     void createTree(int);
+    void levelorder();
 };
 void bst_tree::createTree(int x){
     node *temp=new node;
@@ -46,19 +48,34 @@ void bst_tree::postorder(node *rt){
     postorder(rt->rcl);
     printf("%d ",rt->data);
 }
+void bst_tree::levelorder(){
+    queue <node*> q;
+    node *curr=root;
+    node *temp;
+    q.push(curr);
+    while(!q.empty()){
+        curr=q.front();
+        q.pop();
+        printf("%d ",curr->data);
+        if(curr->lcl)q.push(curr->lcl);
+        if(curr->rcl)q.push(curr->rcl);
+
+    }
+}
 int main(int argc, char const *argv[]) {
     bst_tree l;
     node *temp;
     int menu=1;
     int x;
     while(menu){
-        printf("\n1.insert 2.preorder 3.inorder 4.postorder 0.exit\n");
+        printf("\n1.insert 2.preorder 3.inorder 4.postorder 5.levelorder 0.exit\n");
         scanf("%d",&menu);
         switch (menu) {
             case 1:scanf("%d",&x);l.createTree(x);break;
             case 2:l.preorder(l.getRoot());break;
             case 3:l.inorder(l.getRoot());break;
             case 4:l.postorder(l.getRoot());break;
+            case 5:l.levelorder();break;
         }
     }
     return 0;
