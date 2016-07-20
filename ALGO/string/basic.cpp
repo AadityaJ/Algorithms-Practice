5d0124043a04c1a7db74e05573ef79fc4a755c99
AadityaJ <aadityajamuar@gmail.com>
2016-07-20 22:19:52 +0530

    adding strings

---
+++ ALGO/string/basic.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string>
+
+
+int main(int argc, char const *argv[]) {
+    string s;
+    string sub;
+    scanf("%s",&s);
+    scanf("%s",&sub);
+    for(int i=0;i<s.length();i++){
+        if(s[i])
+        for(int j=0)
+
+
+
+    }
+    return 0;
+}
