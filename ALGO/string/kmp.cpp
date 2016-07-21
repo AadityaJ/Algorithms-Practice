0659ec44ff698e57f36b0afe1559705e7b4eeae7
AadityaJ <aadityajamuar@gmail.com>
2016-07-21 21:07:33 +0530

    adding strings

--- ALGO/string/kmp.cpp
+++ ALGO/string/kmp.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 #include <string>
+void preCalc(string str,string *table){
+    int len=0;
+    int i=1;
+    lps[0]=0;
+    while(i<str.length()){
+        if(str[i]==str[len]){
+            len++;
+            table[i]=i;
+            i++;
+        }
+        else{
+            
+
+
+        }
+
+    }
+
+}
