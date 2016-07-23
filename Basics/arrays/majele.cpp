4f3e85389601b3df238dfd4721b284d4c52c1336
AadityaJ <aadityajamuar@gmail.com>
2016-07-23 15:50:16 +0530

    adding more

--- Basics/arrays/majele.cpp
+++ Basics/arrays/majele.cpp
@@ -3,7 +3,16 @@ int findCand(int arr[],int n){
     int count=1;
     int maj_ele=1;
     for(int i=1;i<n;i++){
-        
+        if(arr[i]==arr[maj_ele]){count++;}
+        else count--;
+        if(!count){
+            count=1;maj_ele=i;
+        }
     }
-    return arr[maj_ele];
+    return isMaj(arr[maj_ele],arr,n);
+}
+int isMaj(int x,int arr[],int size)
+int main(int argc, char const *argv[]) {
+    int arr[]={3,3,4,2,4,4,2,4,4};
+    return 0;
 }
