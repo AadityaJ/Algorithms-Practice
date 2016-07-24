c76f1ed5db67bea2a48caa8427a8f8b000964897
AadityaJ <aadityajamuar@gmail.com>
2016-07-24 20:05:17 +0530

    adding more

--- Basics/arrays/search_rot.c
+++ Basics/arrays/search_rot.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int findMaxIndex(int *arr,int index,int size){
-    if()
+    if(index==0 || index==size-1){return index;}
+    if(arr[index]>arr[index+1] && arr[index]>arr[index-1]){return index;}
+    else if(arr[index]<arr[index-1]){return findMaxIndex(arr,index/2,size);}
+    else return findMaxIndex(arr,(index+size-1)/2,size);
+}
+int main(int argc, char const *argv[]) {
+    int arr[]={3,4,5,1,2};
+    printf("%d\n",findMaxIndex(arr,2,4));
+    return 0;
 }
