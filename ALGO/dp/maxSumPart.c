6df7dab5d00f846130f633d0f932584417f4bd0a
AadityaJ <aadityajamuar@gmail.com>
2016-07-25 20:00:11 +0530

    adding dp

--- ALGO/dp/maxSumPart.c
+++ ALGO/dp/maxSumPart.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
+#define MAX 51
+int arr[MAX][MAX];
+void init(int n,int sum){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<sum;j++)
+            arr[i][j]=-1;
+    }
+}
 int abs(int a){
         return (a>0)?(a):(-a);
 }
-int max(int a,int b){
-    return (a>b)?a:b;
+int min(int a,int b){
+    return (a<b)?a:b;
 }
-int dp(int sum1,int sum2,int arr[],int i,int n){
+int dp(int sum1,int sum2,int array[],int i,int n){
     if(i==n) return abs(sum1-sum2);
+    if(arr[i][sum1]!=-1) return arr[i][sum1];
 
-    return max(dp(sum1+arr[i],sum2,arr,i+1,n),dp(sum1,sum2+arr[i],arr,i+1,n));
+    return arr[i][sum1]=min(dp(sum1+array[i],sum2,array,i+1,n),dp(sum1,sum2+array[i],array,i+1,n));
 }
 int main(int argc, char const *argv[]) {
     int t;
     scanf("%d",&t);
     while(t--){
         int n;
+        //init(n);
         scanf("%d",&n);
         int arr[n];
-        for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+        int sum=0;
+        for(int i=0;i<n;i++) {scanf("%d",&arr[i]);sum+=arr[i];}
+        init(n,sum);
         printf("%d\n",dp(0,0,arr,0,n));
     }
     return 0;
