c44fdf73ec3a4ce46d0b0b348f651444894a0f63
AadityaJ <aadityajamuar@gmail.com>
2016-08-16 19:29:18 +0530

    adding gcj

--- CC/q2/ques.cpp
+++ CC/q2/ques.cpp
@@ -1,40 +1,35 @@
-#include <map>
 #include <iostream>
-#include <string>
-#define MAX 101
 using namespace std;
-int main(int argc, char const *argv[]) {
-    int t;
-    cin>>t;
-    for(int itr=1;itr<=t;itr++){
-        int n;
-        cin>>n;
-        //string str[MAX];
-        char str_char[MAX];
-        string str[MAX];
-        int len=0;
-        for(int i=0;i<=n;i++) cin.getline(str[i],MAX);//cin>>str[i];
-        cout<<str[0];
-        char count[MAX][MAX];
-        for(int i=0;i<MAX;i++){
-            for(int j=0;j<MAX;j++) count[i][j]=0;
-        }
-        //for(int i=0;i<n;i++) count[i]={0};
-        int arr[MAX];
-        for(int i=0;i<n;i++){
-            arr[i]=0;
-            for(int j=0;j<str[i].length();j++){
-                if(!count[i][(str[i][j]-'0')]){
-                    arr[i]++;
-                    count[i][(str[i][j]-'0')]++;
-                }
-            }
-        }
-        int max=0,index=0;
-        for(int i=0;i<n;i++){
-            if(max<arr[i]){max=arr[i];index=i;}
-        }
-        cout<<"Case #"<<itr<<": "<<str[index]<<endl;
-    }
-    return 0;
+int dx[1001][1001];
+void init(){
+	for(int i=0;i<1001;i++){
+		for(int j=0;j<1001;j++) dx[i][j]=-1;
+	}
+}
+int mx(int a,int b,int c){
+	return (a>b)?((a>c)?a:c):((b>c)?b:c);
+}
+int dp(int h,int a,int here){
+	if(h<=0 || a<=0) return 0;
+	if(dx[h][a]!=-1) return dx[h][a];
+	if(here==0){
+		return dx[h][a]=1+max(dp(h+3,a+2,1),dp(h+3,a+2,2));
+	}
+	else if(here==1){
+		return dx[h][a]=1+max(dp(h-5,a-10,0),dp(h-5,a-10,2));
+	}
+	else{
+		return dx[h][a]=1+max(dp(h-20,a+5,0),dp(h-20,a+5,1));
+	}
+}
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+		init();
+		int h,a;
+		cin>>h>>a;
+		cout<<(mx(dp(h,a,0),dp(h,a,1),dp(h,a,2))-1)<<endl;
+	}
+	return 0;
 }
