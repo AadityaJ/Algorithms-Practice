58e5ef167973e72e35fa80f20ae742cfcf45f54f
AadityaJ <aadityajamuar@gmail.com>
2016-08-17 21:45:47 +0530

    adding

--- CC/q2/ques.cpp
+++ CC/q2/ques.cpp
@@ -9,17 +9,17 @@ void init(){
 int mx(int a,int b,int c){
 	return (a>b)?((a>c)?a:c):((b>c)?b:c);
 }
-int dp(int h,int a,int here){
-	if(h<=0 || a<=0) return 0;
+int dp(int h,int a,int here,int count){
+	if(h<=0 || a<=0) return count;
 	if(dx[h][a]!=-1) return dx[h][a];
 	if(here==0){
-		return dx[h][a]=1+max(dp(h+3,a+2,1),dp(h+3,a+2,2));
+		return dx[h][a]=(max(dp(h+3,a+2,1,count+1),dp(h+3,a+2,2,count+1)));
 	}
 	else if(here==1){
-		return dx[h][a]=1+max(dp(h-5,a-10,0),dp(h-5,a-10,2));
+		return dx[h][a]=(max(dp(h-5,a-10,0,count+1),dp(h-5,a-10,2,count+1)));
 	}
 	else{
-		return dx[h][a]=1+max(dp(h-20,a+5,0),dp(h-20,a+5,1));
+		return dx[h][a]=(max(dp(h-20,a+5,0,count+1),dp(h-20,a+5,1,count+1)));
 	}
 }
 int main() {
@@ -29,7 +29,7 @@ int main() {
 		init();
 		int h,a;
 		cin>>h>>a;
-		cout<<(mx(dp(h,a,0),dp(h,a,1),dp(h,a,2))-1)<<endl;
+		cout<<(mx(dp(h,a,0,0),dp(h,a,1,0),dp(h,a,2,0)))<<endl;
 	}
 	return 0;
 }
