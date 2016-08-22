0ef15eb90eb1a97d864f37b07c3e8e18e1e82725
AadityaJ <aadityajamuar@gmail.com>
2016-08-23 00:13:55 +0530

    dp

--- CC/13.cpp
+++ CC/13.cpp
@@ -1,1 +1,24 @@
-#  
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char s[10];
+	int t,n,i,a,w;
+	scanf("%d",&t);
+	while(t--)
+	{
+		w=0;
+		scanf("%d%s",&n,s);
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",&a);
+			if(a%2==0)
+			w++;
+		}
+		if(n==1&&w==1&&strcmp(s,"Dee")==0)
+		printf("Dee\n");
+		else
+		printf("Dum\n");
+	}
+	return 0;
+}
