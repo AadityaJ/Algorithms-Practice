8c251c77f547144d085c1f4e91e86393743c3005
AadityaJ <aadityajamuar@gmail.com>
2017-05-09 21:09:24 +0530

    adding

--- gfgprac/1/d.cpp
+++ gfgprac/1/d.cpp
@@ -4,6 +4,17 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int arr[n+1];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		sort(arr,arr+n);
+		for(int i=n-1;i>=0;i--) cout<<arr[i];
+		cout<<endl;
+	}
 	return 0;
 }
+// 02:00
--- gfgprac/1/e.cpp
+++ gfgprac/1/e.cpp
@@ -2,8 +2,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		int arr[11];
+		for(int i=0;i<10;i++) cin>>arr[i];
+		for(int i=0;i<10;i++){
+			int px=0;
+			for(int j=i+1;j<10;j++){
+				if(arr[i]==arr[j]) px++;
+			}//cout<<px<<endl;
+			if(px==4) {cout<<arr[i]<<endl;break;}
+		}
+	}
 	return 0;
 }
+// 10:00
