7c8984e09903ffc913eeca132e4b904b2a0dc006
AadityaJ <aadityajamuar@gmail.com>
2017-05-10 21:06:54 +0530

    adding

--- gfgprac/2/k.cpp
+++ gfgprac/2/k.cpp
@@ -2,8 +2,16 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		int n,m;
+		cin>>n>>m;
+		int a=(n/pow(2,m-1));
+		cout<<a<<endl;
+	}
 	return 0;
 }
