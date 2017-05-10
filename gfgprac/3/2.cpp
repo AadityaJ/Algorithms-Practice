4c9c6a9074bc2d3a9516a2b64fbd1877322db162
AadityaJ <aadityajamuar@gmail.com>
2017-05-10 23:39:46 +0530

    adding

--- gfgprac/3/2.cpp
+++ gfgprac/3/2.cpp
@@ -7,7 +7,12 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int a,b;
+		cin>>a>>b;
+		int x,y;
+		for(int i=0;i<b;i++) cin>>x>>y;
+		cout<<b<<endl;
 	}
 	return 0;
 }
+// lesson learnt: read very seriously
