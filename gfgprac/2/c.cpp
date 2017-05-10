a364fd6d2b458547312d5fe3d3226bc4e3f1cb76
AadityaJ <aadityajamuar@gmail.com>
2017-05-10 10:09:27 +0530

    adding

--- gfgprac/2/c.cpp
+++ gfgprac/2/c.cpp
@@ -4,6 +4,17 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		for(int i=n;i>0;i--){
+			for(int j=0;j<(n-i);j++) cout<<" ";
+			for(int j=0;j<(2*i-1);j++) cout<<"*";
+			//cout<<endl;
+		}
+		cout<<endl;
+	}
 	return 0;
 }
--- gfgprac/2/d.cpp
+++ gfgprac/2/d.cpp
@@ -4,6 +4,19 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		string n;
+		cin>>n;
+		bool is=1;
+		for(int i=0;i<n.length();i++){
+			if(n[i]!=n[n.length()-i-1]){
+				is=0;break;
+			}
+		}
+		(is)?cout<<"Yes\n":
+			cout<<"No\n";
+	}
 	return 0;
 }
