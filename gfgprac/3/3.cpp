6cbe0cbe96c0479a32f4075149af4cfad6e0ca75
AadityaJ <aadityajamuar@gmail.com>
2017-05-10 23:45:53 +0530

    adding

--- gfgprac/3/3.cpp
+++ gfgprac/3/3.cpp
@@ -7,7 +7,22 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n+1];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		int x;
+		cin>>x;
+		bool isPresent=0;
+		int f=0,l=n-1;
+		for(int i=0;i<n;i++){
+			if(arr[i]==x){f=i;isPresent=1;break;}
+		}
+		for(int i=n-1;i>=0;i--){
+			if(arr[i]==x){l=i;isPresent=1;break;}
+		}
+		if(isPresent) cout<<f<<" "<<l<<endl;
+		else cout<<"-1"<<endl;
 	}
 	return 0;
 }
