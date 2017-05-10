2e85811eeba97363b6358c628b3e5cc2b78da4e5
AadityaJ <aadityajamuar@gmail.com>
2017-05-10 21:22:56 +0530

    adding

--- gfgprac/2/l.cpp
+++ gfgprac/2/l.cpp
@@ -4,6 +4,18 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int j=1;
+		for (int i = 1; i <=n; ++i){
+			if(i==(j*j)){cout<<"1";j++;}
+			else cout<<"0";
+			cout<<" ";
+		}
+		cout<<endl;
+	}
 	return 0;
 }
--- gfgprac/2/m.cpp
+++ gfgprac/2/m.cpp
@@ -2,8 +2,50 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cstring>
 using namespace std;
+// dumb and stupid
+/*
+int lcs( string X, string Y, int m, int n )
+{
+   int L[m+1][n+1];
+   int i, j;
+   for (i=0; i<=m; i++)
+   {
+     for (j=0; j<=n; j++)
+     {
+       if (i == 0 || j == 0)
+         L[i][j] = 0;
+  
+       else if (X[i-1] == Y[j-1])
+         L[i][j] = L[i-1][j-1] + 1;
+  
+       else
+         L[i][j] = max(L[i-1][j], L[i][j-1]);
+     }
+   }
+   return L[m][n];
+}
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}*/
+bool lcs(string s1,string s2){
+	int s1_index=0;
+	for(int i=0;i<s2.length();i++){
+		if(s1[s1_index]==s2[i]) s1_index++;
+	}
+	//cout<<s1_index;
+	return (s1_index==s1.length());
+}
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		string s1,s2;
+		cin>>s1>>s2;
+		//int x=lcs(s1,s2);
+		cout<<lcs(s1,s2)<<endl;
+	}
 	return 0;
 }
