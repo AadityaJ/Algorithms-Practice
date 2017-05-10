ee6e43a615b72c34c466641dce4b578dc18faa78
AadityaJ <aadityajamuar@gmail.com>
2017-05-10 12:14:26 +0530

    adding

--- gfgprac/2/i.cpp
+++ gfgprac/2/i.cpp
@@ -4,6 +4,21 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int arr[n];
+		int mx=0,mx2=0;
+		for(int i=0;i<n;i++) cin>>arr[i];
+		for(int i=0;i<n;i++){
+			if(arr[i]>mx){mx=arr[i];}
+		}
+		for(int i=0;i<n;i++){
+			if(arr[i]>mx2 && arr[i]!=mx){mx2=arr[i];}
+		}
+		cout<<mx2<<endl;
+	}
 	return 0;
 }
--- gfgprac/2/j.cpp
+++ gfgprac/2/j.cpp
@@ -4,6 +4,20 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		for(int i=0;i<n-1;i++){
+			if(arr[i]>arr[i+1]) cout<<arr[i+1];
+			else cout<<"-1";
+			cout<<" ";
+		}
+		cout<<"-1\n";
 
+	}
 	return 0;
 }
