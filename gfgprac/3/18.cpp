cd21d771502ccbaca6a4858de64c8813d46fbd84
AadityaJ <aadityajamuar@gmail.com>
2017-05-12 00:09:33 +0530

    adding

--- gfgprac/3/18.cpp
+++ gfgprac/3/18.cpp
@@ -2,12 +2,16 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		((n&(n-1))==0)?(cout<<(int)((log(n)/log(2)))+1):(cout<<"-1");
+		cout<<endl;
 	}
 	return 0;
 }
