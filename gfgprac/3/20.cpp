cac6c18fa07929674a7973edc18f5a21edec7ce4
AadityaJ <aadityajamuar@gmail.com>
2017-05-12 00:43:02 +0530

    adding

--- gfgprac/3/20.cpp
+++ gfgprac/3/20.cpp
@@ -2,12 +2,26 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		queue <string> st;
+		string str="1";
+		st.push(str);
+		for(int i=1;i<=n;i++){
+			str=st.front();
+			cout<<str<<" ";
+			//cout<<(str+"0")<<" ";
+			st.pop();
+			st.push(str+"0");
+			st.push(str+"1");
+		}//cout<<st.size()<<endl;
+		cout<<endl;
 	}
 	return 0;
 }
