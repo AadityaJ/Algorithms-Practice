86e42a43e7b108d3f8c9d09d6efc0ce39838c6fd
AadityaJ <aadityajamuar@gmail.com>
2017-05-12 00:13:23 +0530

    adding

--- gfgprac/3/19.cpp
+++ gfgprac/3/19.cpp
@@ -2,12 +2,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <climits>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		sort(arr,arr+n);
+		int mn=INT_MAX;
+		for(int i=1;i<n;i++){
+			if(mn>(arr[i]-arr[i-1])) mn=arr[i]-arr[i-1];
+		}
+		cout<<mn<<endl;
 	}
 	return 0;
 }
--- push.sh
+++ push.sh
@@ -1,4 +1,3 @@
 git add .
 git commit -m "adding"
 git push origin master
-^L
