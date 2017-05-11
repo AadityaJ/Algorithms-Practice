bea75259c434ab07a939a22005608822447d3f1d
AadityaJ <aadityajamuar@gmail.com>
2017-05-11 19:40:30 +0530

    adding

--- gfgprac/3/12.cpp
+++ gfgprac/3/12.cpp
@@ -7,7 +7,22 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n];
+		int temp[n];	
+		for(int i=0;i<n;i++) {cin>>arr[i];temp[i]=0;}
+		for(int i=0;i<n;i++){
+			int count=0;
+			for(int j=i+1;j<n;j++){
+				if(arr[i]<arr[j]) count++;
+			}
+			cout<<count<<" ";
+		}
+		cout<<endl;
 	}
 	return 0;
 }
+
+
+// TODO: http://www.geeksforgeeks.org/counting-inversions/
