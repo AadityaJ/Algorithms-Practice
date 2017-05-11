70f490bf88d46274f2fda1c15671a7abfa7e888e
AadityaJ <aadityajamuar@gmail.com>
2017-05-11 12:05:36 +0530

    adding

--- gfgprac/3/5.cpp
+++ gfgprac/3/5.cpp
@@ -3,11 +3,25 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int rec(int N){
+	for (int i = 0; i < 2 * N - 1; i++) {
+        int k = i < N ? i : 2 * (N - 1) - i; 
+        for (int j = 0; j < k; j++)
+            cout << N - j;
+        for (int j = 0; j < 2 * (N - k) - 1; j++)
+            cout << N - k;
+        for (int j = k - 1; j >= 0; j--)
+            cout << N - j;
+		cout << endl;
+    }
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		rec(n);
 	}
 	return 0;
 }
--- gfgprac/3/6.cpp
+++ gfgprac/3/6.cpp
@@ -3,11 +3,37 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int rev(string x){
+	/*string x="";
+	int j=a;
+	while(j){
+		x+=a+'0';
+		j/=10;
+	}*/
+	reverse(x.begin(),x.end());
+	int j=0;
+	for(int i=0;i<x.length();i++){
+		j+=(x[i]-'0');
+		j*=10;
+	}
+	return j/10;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str;
+		cin>>str;
+		//cout<<str<<" "<<str.length()<<endl;
+		bool is=0;
+		string num="";
+		int i;
+		for(i=str.length()-1;(str[i])<='9' && str[i]>='0';i--){
+			num+=str[i];
+		}
+		int x=rev(num)-1;
+		//cout<<str<<" "<<x<<" "<<i<<endl;
+		cout<<(i==x)<<endl;
 	}
 	return 0;
 }
