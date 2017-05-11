694cfd0226262b06dbed9043f36d84b73124002c
AadityaJ <aadityajamuar@gmail.com>
2017-05-11 18:50:10 +0530

    adding

--- gfgprac/3/11.cpp
+++ gfgprac/3/11.cpp
@@ -2,11 +2,49 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
+int chmp[26];
+void init(){
+	for(int i=0;i<26;i++) chmp[i]=0;
+}
+void calcFreq(string str){
+	for(int i=0;i<str.length();i++){
+		chmp[str[i]-'a']++;
+	}
+}
+int calc(string str,int k){
+	if(str.length()<=k) return 0;
+	priority_queue<int> pq;
+	for(int i=0;i<26;i++){
+		//cout<<i+'a'<<" "<<chmp[i]<<endl;
+		pq.push(chmp[i]);
+	}
+	for(int i=0;i<k;i++){
+		int x=pq.top();
+		pq.pop();
+		x--;
+		pq.push(x);
+	}
+	int val=0;
+	while(!pq.empty()){
+		int x=pq.top();
+		pq.pop();
+		val+=(x*x);
+	}
+	return val;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
+		init();
+		string str;
+		cin>>str;
+		calcFreq(str);
+		int k;
+		cin>>k;
+		cout<<calc(str,k)<<endl;
 
 	}
 	return 0;
--- gfgprac/3/9.cpp
+++ gfgprac/3/9.cpp
@@ -7,7 +7,14 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int x=0;
+		while(n){
+			x+=(n%2);
+			n/=2;
+		}
+		cout<<x<<endl;
 	}
 	return 0;
 }
