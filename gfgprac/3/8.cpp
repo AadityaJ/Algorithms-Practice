94dbab879aefd30b251861c6b148980f4c9b1cfb
AadityaJ <aadityajamuar@gmail.com>
2017-05-11 12:50:01 +0530

    adding

--- gfgprac/3/8.cpp
+++ gfgprac/3/8.cpp
@@ -2,12 +2,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <climits>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n+1];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		sort(arr,arr+n);
+		int mn=INT_MAX;
+		int k;
+		cin>>k;
+		for(int i=k-1;i<n;i++){
+			if((arr[i]-arr[i-k+1])<mn) mn=(arr[i]-arr[i-k+1]);
+		}
+		cout<<mn<<endl;
 	}
 	return 0;
 }
