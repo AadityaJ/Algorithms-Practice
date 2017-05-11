8e71d68ddcac5f1cf4623e7ada58d86446e315bf
AadityaJ <aadityajamuar@gmail.com>
2017-05-11 21:13:45 +0530

    adding

--- gfgprac/3/16.cpp
+++ gfgprac/3/16.cpp
@@ -7,7 +7,24 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		int cnt=0;
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				if((arr[i]+arr[j])%3==0) {cnt++;}
+			}
+		}
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				for(int k=j+1;k<n;k++){
+					if((arr[i]+arr[j]+arr[k])%3==0)  {cnt++;}
+				}
+			}
+		}
+		cout<<cnt<<endl;
 	}
 	return 0;
 }
