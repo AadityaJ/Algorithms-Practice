76aa5d510abd064c0b2311de16b78085a1ba05c9
AadityaJ <aadityajamuar@gmail.com>
2017-05-11 12:25:07 +0530

    adding

--- gfgprac/3/7.cpp
+++ gfgprac/3/7.cpp
@@ -2,12 +2,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		if(n==1) {cout<<"1"<<endl;continue;}
+		bool is=0;
+		for(int i=2;i<n;i++){
+			if(n%i) continue;
+			for(int j=1;j<n;j++)
+			if(n==pow(i,j)){is=1;break;}
+		}
+		cout<<is<<endl;
 	}
 	return 0;
 }
