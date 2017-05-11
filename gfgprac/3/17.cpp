1ceb6c4451dad1b1b2cc944c9beae5a2b3cc65b8
AadityaJ <aadityajamuar@gmail.com>
2017-05-11 23:57:48 +0530

    adding:

--- gfgprac/3/17.cpp
+++ gfgprac/3/17.cpp
@@ -3,11 +3,23 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+long long int fib[81];
+void init(){
+	fib[0]=1;
+	fib[1]=1;
+	for(int i=2;i<81;i++){
+		fib[i]=fib[i-1]+fib[i-2];
+	}
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
+	init();
 	while(t--){
-
+		int n;
+		cin>>n;
+		cout<<fib[n]<<endl;
 	}
 	return 0;
 }
+//https://www.dartmouth.edu/~academicoutreach/docs/fibonacci-numbers-and-tiling.pdf
