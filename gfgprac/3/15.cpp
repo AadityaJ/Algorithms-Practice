0b4bbf75344692127add6233f73fa9a4250b4daa
AadityaJ <aadityajamuar@gmail.com>
2017-05-11 20:44:33 +0530

    adding

--- gfgprac/3/15.cpp
+++ gfgprac/3/15.cpp
@@ -2,12 +2,37 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str,ans="";
+		cin>>str;
+		int N=0,S=0,E=0,W=0;
+		for(int i=0;i<str.length();i++){
+			if(str[i]=='N') N++;
+			if(str[i]=='S') S++;
+			if(str[i]=='E') E++;
+			if(str[i]=='W') W++;
+		}
+		int y=abs(N-S);
+		int x=abs(E-W);
+		if(N>S){
+			for(int i=0;i<y;i++) ans+='N';
+		}
+		else{
+			for(int i=0;i<y;i++) ans+='S';
+		}
+		if(E>W){
+			for(int i=0;i<x;i++) ans+='E';
+		}
+		else{
+			for(int i=0;i<x;i++) ans+='W';
+		}
+		sort(ans.begin(),ans.end());
+		cout<<ans<<endl;
 	}
 	return 0;
 }
