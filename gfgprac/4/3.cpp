c91e6e6fad13fa842622fadc3f6b297450985917
AadityaJ <aadityajamuar@gmail.com>
2017-05-12 12:03:39 +0530

    adding

--- gfgprac/4/3.cpp
+++ gfgprac/4/3.cpp
@@ -7,7 +7,42 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		cin>>n;
+		 string temp;
+		 vector<string> v;
+		 for(i=0;i<n;i++)
+		 {
+			 cin>>temp;
+			 v.push_back(temp);
+		 }
+		 map<string,int> m;
+		 vector<int> final;
+		 int count=0;
+		 for(i=0;i<n;i++)
+		 {
+			 if(m.find(v[i])==m.end())
+			 {
+				 m[v[i]]=1;
+				 //if(v[i].find('1')!=string::npos)
+				 for(j=0;j<v[i].length();j++)
+				 {
+					 if(v[i][j]=='1')
+					   break;
+				 }
+				 if(j<n)
+				 count++;
+				 //final.push_back(i);
+			 }
+		 }
+		 for(i=0;i<count;i++)
+		 {
+			 cout<<i<<" ";
+			 //cout<<final[i]<<" ";
+		 }
+		 if(count==0)
+		   cout<<-1;
+		 cout<<"\n";
+	 }
 	}
 	return 0;
 }
