f088a6299c7e1338c38df1b5a0190d5b62bafaa2
AadityaJ <aadityajamuar@gmail.com>
2017-05-13 11:53:07 +0530

    adding

--- gfgprac/4/9.cpp
+++ gfgprac/4/9.cpp
@@ -2,12 +2,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
+bool isInt(float x){
+	return (ceilf(x)==x)&&(floorf(x)==x);
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n,m;
+		cin>>n>>m;
+		//cout<<log10(m)<<endl;
+		double x=log10(m)/n;
+		//cout<<x<<endl;
+		double ans=pow(10,x);
+		//cout<<ans<<endl;
+		//cout<<(ceilf(ans))<<endl;
+		(isInt(ans))?(cout<<ans<<endl):(cout<<"-1"<<endl);
 	}
 	return 0;
 }
