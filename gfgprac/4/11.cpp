c13d06af7606ec8b29e0930305c53443c85bc286
AadityaJ <aadityajamuar@gmail.com>
2017-05-13 21:05:18 +0530

    adding

--- gfgprac/4/11.cpp
+++ gfgprac/4/11.cpp
@@ -7,7 +7,21 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str;
+		cin>>str;
+		int ope=0,clo=0;
+		for(int i=0;i<str.length();i++){
+			if(str[i]==')') clo++;
+		}
+		int ans=-1;
+		int i;
+		for(i=0;i<str.length();i++){
+			if(ope==clo){ans=i;break;}
+			if(str[i]=='(') ope++;
+			else clo--;
+		}
+		if(ope==clo && i==str.length()) ans=str.length();
+		cout<<ans<<endl;
 	}
 	return 0;
 }
