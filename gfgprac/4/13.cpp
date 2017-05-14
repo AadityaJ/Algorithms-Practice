bc7a3cd3ff97abc248febad2a97774ce7e5d14b4
AadityaJ <aadityajamuar@gmail.com>
2017-05-14 19:08:34 +0530

    adding

--- gfgprac/4/13.cpp
+++ gfgprac/4/13.cpp
@@ -2,12 +2,30 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str;
+		cin>>str;
+		string ans="";
+		stack <int> s1;
+		stack <string> s2;
+		string tmp="";
+		for(int i=0;i<str.length();i++){
+			if(str[i]>='0' &&str[i]<='9') {s1.push(str[i]-'0');s2.push(tmp);tmp="";cout<<"s1: "<<str[i]<<endl;}
+			else if(str[i]==']'){
+				s2.push(tmp);
+				cout<<"s2: "<<tmp<<endl;
+				tmp="";
+			}
+			else if(str[i]>='a'&&str[i]<='z'){
+				tmp+=str[i];
+			}
+		}
+		
 	}
 	return 0;
 }
--- gfgprac/4/14.cpp
+++ gfgprac/4/14.cpp
@@ -3,11 +3,31 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+bool isEven(char a){
+	if(a=='0') return 1;
+	if(a=='2') return 1;
+	if(a=='4') return 1;
+	if(a=='6') return 1;
+	if(a=='8') return 1;
+	return 0;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str;
+		cin>>str;
+		string ans="-1";
+		for(int i=str.length()-1;i>=0;i--){
+			cout<<isEven(str[i])<<endl;
+			if(isEven(str[i]) &&(str[i]<str[str.length()-1])){
+				char tmp=str[i];
+				str[i]=str[str.length()-1];
+				str[str.length()-1]=tmp;
+				ans=str;break;
+			}
+		}
+		cout<<ans<<endl;
 	}
 	return 0;
 }
