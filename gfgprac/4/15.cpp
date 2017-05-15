6abea0a8f7051772c4ea751c0f119b85d561b5f2
AadityaJ <aadityajamuar@gmail.com>
2017-05-15 21:37:31 +0530

    adding

--- gfgprac/4/15.cpp
+++ gfgprac/4/15.cpp
@@ -3,11 +3,27 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+bool isEven(char c){
+	int a=c-'0';
+	return a%2==0;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str;
+		cin>>str;
+		//next_permutation(str.begin(),str.end());
+		//cout<<str<<endl;
+		string ans="-1";
+		while(next_permutation(str.begin(),str.end())){
+			if(isEven(str[str.length()-1])){
+				ans=str;
+				break;
+			}
+		}
+		cout<<ans<<endl;
 	}
 	return 0;
 }
+// know next_permutation
