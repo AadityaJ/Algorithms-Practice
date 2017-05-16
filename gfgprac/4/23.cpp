92dc65f453746a572a1b0c031b8ee4015cbf7285
AadityaJ <aadityajamuar@gmail.com>
2017-05-17 02:41:53 +0530

    adding

--- gfgprac/4/23.cpp
+++ gfgprac/4/23.cpp
@@ -7,7 +7,24 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		n=(2*n)+2;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		int a=arr[0];
+		for(int i=1;i<n;i++){
+			a^=arr[i];
+		}
+		int x=a& ~(a-1);
+		int val1=0,val2=0;
+		for(int i=0;i<n;i++){
+			if(arr[i]&x) val1^=arr[i];
+			else val2^=arr[i];
+		}
+		int a1=min(val1,val2);
+		int a2=max(val1,val2);
+		cout<<a1<<" "<<a2<<endl;
 	}
 	return 0;
 }
--- gfgprac/4/24.cpp
+++ gfgprac/4/24.cpp
@@ -7,7 +7,23 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		int cnt=0;
+		for(int i=0;i<n;i++) if(arr[i]==1) cnt++;
+		int i=0;
+		int mx=0;
+		while(i<n){
+			int x_cnt=0;
+			if(arr[i]==0){
+				while(arr[i++]!=1 && i<n) x_cnt++;
+			}
+			if(x_cnt>mx) mx=x_cnt;
+			i++;
+		}
+		cout<<(mx+cnt)<<endl;
 	}
 	return 0;
 }
