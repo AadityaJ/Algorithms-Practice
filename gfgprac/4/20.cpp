b463599328373c4b1496ff721283c9a6eea0c6b0
AadityaJ <aadityajamuar@gmail.com>
2017-05-17 00:45:06 +0530

    adding

--- gfgprac/4/20.cpp
+++ gfgprac/4/20.cpp
@@ -7,7 +7,20 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str;
+		cin>>str;
+		//std::cout << str[0] << std::endl;
+		int i=0,cnt=0;
+		while(i<str.length()){
+			if(str[i++]=='1'){
+				if(str[i]=='0'){
+					while(str[i++]=='0');
+					if(str[i-1]=='1'){cnt++;i--;}
+				}
+			}
+			//i++;
+		}
+		cout<<cnt<<endl;
 	}
 	return 0;
 }
--- gfgprac/4/21.cpp
+++ gfgprac/4/21.cpp
@@ -3,11 +3,27 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int sumr(int *arr,int n,int x){
+	int sum=0;
+	for(int i=x;i<n;i++) sum+=arr[i];
+	return sum;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		int sum_l=0,sum_r=sumr(arr,n,1);
+		int ans=-1;
+		for(int i=0;i<n;i++){
+			if(sum_l==sum_r){ans=++i;break;}
+			sum_l+=arr[i];
+			sum_r-=arr[i+1];
+		}
+		cout<<(ans)<<endl;
 	}
 	return 0;
 }
