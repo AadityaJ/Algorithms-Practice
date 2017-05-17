22c1c13a86cfae2417158f8911e3325514483e5d
AadityaJ <aadityajamuar@gmail.com>
2017-05-18 00:35:56 +0530

    adding

--- gfgprac/4/29.cpp
+++ gfgprac/4/29.cpp
@@ -2,12 +2,29 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
+#include <string.h>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		char str[2001];
+		cin>>str;
+		stack <string> st;
+		char *ptr;
+		ptr=strtok(str,".");
+		while(ptr){
+			st.push(ptr);
+			ptr=strtok(NULL,".");
+		}
+		while(!st.empty()){
+			cout<<st.top();
+			if(st.size()!=1) cout<<".";
+			st.pop();
+		}
+		cout<<endl;
 	}
 	return 0;
 }
+// strtok :: learn and weird
