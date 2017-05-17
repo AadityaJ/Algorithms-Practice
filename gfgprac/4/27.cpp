b30d1bb5323d06438527ceb08e5a0f99f9092503
AadityaJ <aadityajamuar@gmail.com>
2017-05-17 23:36:22 +0530

    adding

--- gfgprac/4/27.cpp
+++ gfgprac/4/27.cpp
@@ -3,11 +3,27 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+void rec(string str,int i,string ans){
+	if(i==str.length()){
+		if(ans[0]!=' ') cout<<"("<<ans<<")";
+		return ;
+	}
+
+	string n=ans;
+	n.insert(i," ");
+	cout<<n<<endl;
+	rec(str,i+2,n);
+	rec(str,i+1,ans);
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str;
+		cin>>str;
+		string ans="";
+		rec(str,1,str);
+		cout<<endl;
 	}
 	return 0;
 }
