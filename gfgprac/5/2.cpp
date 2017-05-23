eb558c42fae58261c4784f43417859703a761fb9
AadityaJ <aadityajamuar@gmail.com>
2017-05-23 12:03:00 +0530

    adding

--- gfgprac/5/2.cpp
+++ gfgprac/5/2.cpp
@@ -7,10 +7,13 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-		int n;
+		string n;
 		cin>>n;
-		int arr[n];
-
+		int hash[26];
+		for(int i=0;i<26;i++) hash[i]=0;
+		for(int i=n.length();i>=1;i--){
+			
+		}
 	}
 	return 0;
 }
