67346da43bb7e6bf1ad89b399b9e048c4759d189
AadityaJ <aadityajamuar@gmail.com>
2017-05-23 12:25:07 +0530

    adding

--- gfgprac/5/2.cpp
+++ gfgprac/5/2.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+void init(int *hash){
+	for(int i=0;i<26;i++) hash[i]=0;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
@@ -10,9 +13,17 @@ int main(int argc, char const *argv[]){
 		string n;
 		cin>>n;
 		int hash[26];
-		for(int i=0;i<26;i++) hash[i]=0;
-		for(int i=n.length();i>=1;i--){
-			
+		int i;
+		//for(i=0;i<26;i++) hash[i]=0;
+		for(i=n.length();i>=1;i--){
+			init(hash);
+			for(int x=0;x<i;x++){
+				hash[x-'a']++;
+			}
+			for(int x=i-1;x<n.length();x++){
+				hash[x-'a']=1;
+
+			}
 		}
 	}
 	return 0;
