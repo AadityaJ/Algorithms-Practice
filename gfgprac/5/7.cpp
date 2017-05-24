c805f88ecdb3feea3de997a457b1a33e633a9519
AadityaJ <aadityajamuar@gmail.com>
2017-05-24 14:39:24 +0530

    adding

--- gfgprac/5/7.cpp
+++ gfgprac/5/7.cpp
@@ -2,12 +2,33 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define L 1000001
 using namespace std;
+int arr[L];
+bool is(int n){
+	int x=(n%10);
+	n/=10;
+	while(n){
+		if((n%10)-x!=1 && x-(n%10)!=1) return 0;
+		n/=10;
+	}
+	return 1;
+}
+void init(){
+	arr[0]=1;
+	for(int i=1;i<L;i++){
+		if(is(i)) arr[i]=(arr[i-1]+1);
+		else arr[i]=(arr[i-1]);
+	}
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
+	init();
 	while(t--){
-
+		int a,b;
+		cin>>a>>b;
+		cout<<(arr[b]-arr[a]+is(a))<<endl;
 	}
 	return 0;
 }
