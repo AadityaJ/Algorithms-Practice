a30fe38fa7711a21e8dd5c042fd31556de196431
AadityaJ <aadityajamuar@gmail.com>
2017-05-25 00:37:13 +0530

    adding

--- gfgprac/5/12.cpp
+++ gfgprac/5/12.cpp
@@ -3,11 +3,24 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int calc(int n,int k,int *arr,int sum){
+	if(sum>=k) return sum==k;
+	int x=0;
+	for(int i=0;i<n;i++){
+		x+=calc(n,k,arr,sum+arr[i]);
+	}
+	return x;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int m,n;
+		cin>>m;
+		int arr[m];
+		for(int i=0;i<m;i++) cin>>arr[i];
+		cin>>n;
+		cout<<calc(m,n,arr,0)<<endl;
 	}
 	return 0;
 }
