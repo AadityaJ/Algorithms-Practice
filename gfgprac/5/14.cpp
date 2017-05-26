e832bd8a08835a2a907974bbc993baa86d2e609b
AadityaJ <aadityajamuar@gmail.com>
2017-05-26 22:21:53 +0530

    adding

--- gfgprac/5/14.cpp
+++ gfgprac/5/14.cpp
@@ -2,12 +2,23 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str;
+		cin>>str;
+		//map<string,int> mp;
+		for(int i=0;i<str.length();i++){
+			string s="";
+			s+=str[i];
+			for(int j=i;j>=0;j--){
+				cout<<i<<" "<<j<<" "<<s<<endl;
+				s+=str[j];
+			}
+		}
 	}
 	return 0;
 }
