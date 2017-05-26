2f9088be37437a6d4cb5b3c685c881d1975f1fa9
AadityaJ <aadityajamuar@gmail.com>
2017-05-26 21:16:21 +0530

    adding

--- gfgprac/5/13.cpp
+++ gfgprac/5/13.cpp
@@ -2,12 +2,36 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define MAX 301
 using namespace std;
+int dp[MAX][MAX];
+void init(){
+	for(int i=0;i<MAX;i++){
+		for(int j=0;j<MAX;j++) dp[i][j]=-1;
+	}
+}
+int calc(int *arr,int n,int i,int sum,int k){
+	if(dp[i][sum]!=-1) return dp[i][sum];
+	if(sum>=k) return sum==k;
+	if(i>=n) return 0;
+	int ans=0;
+	for(int x=i;x<n;x++){
+		ans+=calc(arr,n,x,sum+arr[x],k);
+	}
+	return dp[i][sum]=ans;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		init();
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		int k;
+		cin>>k;
+		cout<<calc(arr,n,0,0,k)<<endl;
 	}
 	return 0;
 }
