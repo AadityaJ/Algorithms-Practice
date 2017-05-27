b860372008d13384c3092f1f6d460130a0421fe5
AadityaJ <aadityajamuar@gmail.com>
2017-05-27 13:41:42 +0530

    adding

--- gfgprac/5/15.cpp
+++ gfgprac/5/15.cpp
@@ -2,12 +2,31 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define MAX 1001
 using namespace std;
+int mp[MAX];
+void init(){
+	for(int i=0;i<MAX;i++) mp[i]=0;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		init();
+		int n,m;
+		cin>>n>>m;
+		int a1[n],a2[m];
+		for(int i=0;i<n;i++) cin>>a1[i];
+		for(int i=0;i<m;i++) cin>>a2[i];
+		for(int i=0;i<n;i++) mp[a1[i]]++;
+		for(int i=0;i<m;i++){
+			for(int j=0;j<mp[a2[i]];j++) {cout<<a2[i]<<" ";}
+			mp[a2[i]]=0;
+		}
+		for(int i=0;i<MAX;i++){
+			for(int j=0;j<mp[i];j++) cout<<i<<" ";
+		}
+		cout<<endl;
 	}
 	return 0;
 }
