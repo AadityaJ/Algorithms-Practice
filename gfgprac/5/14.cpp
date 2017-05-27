0fac2f4dfcc63ab83c5097443296bbe31db7936d
AadityaJ <aadityajamuar@gmail.com>
2017-05-27 13:11:28 +0530

    adding

--- gfgprac/5/14.cpp
+++ gfgprac/5/14.cpp
@@ -3,22 +3,29 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#define MAX 100
 using namespace std;
+int dp[MAX][2];
+void init(){
+	for(int i=0;i<MAX;i++){
+		dp[i][0]=-1;
+		dp[i][1]=-1;
+	}
+}
+int calc(string str,int n,int i){
+	if(str.length()==n) {return 1;}
+	if(dp[i][str[i-1]=='1']!=-1) return dp[i][str[i-1]=='1'];
+	if(str[i-1]=='1') return dp[i][str[i-1]=='1']=calc(str+"0",n,i+1);
+	else return dp[i][str[i-1]=='1']=calc(str+"0",n,i+1)+calc(str+"1",n,i+1);
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-		string str;
-		cin>>str;
-		//map<string,int> mp;
-		for(int i=0;i<str.length();i++){
-			string s="";
-			s+=str[i];
-			for(int j=i;j>=0;j--){
-				cout<<i<<" "<<j<<" "<<s<<endl;
-				s+=str[j];
-			}
-		}
+		init();
+		int n;
+		cin>>n;
+		cout<<calc("0",n,1)+calc("1",n,1)<<endl;
 	}
 	return 0;
 }
