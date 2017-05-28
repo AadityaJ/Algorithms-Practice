ef9da995f7f0e63ab3b798afb079c3b6e7f873aa
AadityaJ <aadityajamuar@gmail.com>
2017-05-28 12:06:24 +0530

    adding

--- gfgprac/5/16.cpp
+++ gfgprac/5/16.cpp
@@ -2,12 +2,30 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define MAX 1000
 using namespace std;
+int F[MAX][MAX];
+void init(){
+	for(int i=0;i<MAX;i++){
+		for(int j=0;j<MAX;j++) F[i][j]=-1;
+	}
+}
+int calc(int *arr,int i,int j){
+	if(i==j) return arr[i];
+	if(i+1==j) return max(arr[i],arr[j]);
+	if(F[i][j]!=-1) return F[i][j];
+	return F[i][j]=max(arr[i]+min(calc(arr,i+2,j),calc(arr,i+1,j-1)),arr[j]+min(calc(arr,i,j-2),calc(arr,i+1,j-1)));
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		init();
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		cout<<calc(arr,0,n-1)<<endl;
 	}
 	return 0;
 }
