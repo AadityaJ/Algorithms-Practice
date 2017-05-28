c1fefdc296aaf46eae5443222059d2e68decc595
AadityaJ <aadityajamuar@gmail.com>
2017-05-28 20:05:07 +0530

    adding

--- gfgprac/5/17.cpp
+++ gfgprac/5/17.cpp
@@ -4,10 +4,20 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]){
-	int t;
-	cin>>t;
-	while(t--){
-
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+		int key;
+		cin>>key;
+		int j=i-1;
+		while(j>=0 && a[j]>key){
+			a[j+1]=a[j];
+			j--;
+		}
+		a[j+1]=key;
+		if(i%2) cout<<(a[i/2]+a[i/2+1])/2<<endl;
+		else cout<<a[i/2]<<endl;
 	}
 	return 0;
 }
