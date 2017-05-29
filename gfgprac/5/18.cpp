ebbfc1340ea5be771aab0efb3f2e473b82bcf660
AadityaJ <aadityajamuar@gmail.com>
2017-05-29 21:42:22 +0530

    adding

--- gfgprac/5/18.cpp
+++ gfgprac/5/18.cpp
@@ -2,12 +2,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		long long n;
+		cin>>n;
+		long long ans=1;
+		long long d=2;
+		while(n>1){
+			while(n%d==0){
+				n/=d;
+				ans=d;
+			}
+			d=d+1;
+			if(d*d>n){
+				if(n>1) {ans=n;break;}
+			}
+		}
+		cout<<ans<<endl;
 	}
 	return 0;
 }
