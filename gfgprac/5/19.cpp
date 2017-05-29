5c4df89dcf7dfba9a93e47319bae547144665ca1
AadityaJ <aadityajamuar@gmail.com>
2017-05-29 21:54:36 +0530

    adding

--- gfgprac/5/19.cpp
+++ gfgprac/5/19.cpp
@@ -2,11 +2,16 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <"/Users/aaditya/Desktop/Algorithms-Practice/lib/stdc++.h">
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
 
 	}
 	return 0;
