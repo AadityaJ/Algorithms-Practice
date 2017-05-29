ffb5c8c62758c0db4b6d3d07445310732cca421e
AadityaJ <aadityajamuar@gmail.com>
2017-05-29 22:42:36 +0530

    adding

--- gfgprac/5/19.cpp
+++ gfgprac/5/19.cpp
@@ -2,17 +2,34 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <"/Users/aaditya/Desktop/Algorithms-Practice/lib/stdc++.h">
+#include <cmath>
+#define MAX 5000
 using namespace std;
+int dp[100][MAX];
+void init(){
+	for(int i=0;i<100;i++){
+		for(int j=0;j<MAX;j++) dp[i][j]=-1;
+	}
+}
+int calc(int *arr,int i,int n,int s1,int sum){
+	if(dp[i][s1]!=-1) return dp[i][s1];
+	if(i==n) {
+		int s2=(sum-s1);
+		return abs(s1-s2);
+	}
+	return dp[i][s1]=min(calc(arr,i+1,n,s1+arr[i],sum),calc(arr,i+1,n,s1,sum));
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
+		init();
 		int n;
 		cin>>n;
 		int arr[n];
-		for(int i=0;i<n;i++) cin>>arr[i];
-
+		int sum=0;
+		for(int i=0;i<n;i++) {cin>>arr[i];sum+=arr[i];}
+		cout<<calc(arr,0,n,0,sum)<<endl;
 	}
 	return 0;
 }
