bae87aaafb9ce4b82c7b97553777bc14ad7bf540
AadityaJ <aadityajamuar@gmail.com>
2017-05-30 20:50:51 +0530

    adding

--- gfgprac/5/21.cpp
+++ gfgprac/5/21.cpp
@@ -7,7 +7,25 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		if(n==1){cout<<"1\n";continue;}
+		string ans="1";
+		for(int i=2;i<=n;i++){
+			ans+='$';
+			string temp="";
+			int cnt=1;
+			for(int j=1;j<ans.length();j++){
+				if(ans[j]!=ans[j-1]){
+					temp+=(cnt+'0');
+					temp+=(ans[j-1]);
+					cnt=1;
+				}
+				else cnt++;
+			}
+			ans=temp;
+		}
+		cout<<ans<<endl;
 	}
 	return 0;
 }
