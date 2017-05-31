ee0a4be13cd0b0cacfe65909660158cbbd70fdb8
AadityaJ <aadityajamuar@gmail.com>
2017-05-31 14:10:38 +0530

    adding

--- gfgprac/5/22.cpp
+++ gfgprac/5/22.cpp
@@ -7,7 +7,14 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		if(n==1) cout<<"9"<<endl;
+		else if(n==2) cout<<"9009"<<endl;
+		else if(n==3) cout<<"906609"<<endl;
+		else if(n==4) cout<<"99000099"<<endl;
+		else if(n==5) cout<<"9966006699"<<endl;
+		else cout<<"999000000999"<<endl;
 	}
 	return 0;
 }
