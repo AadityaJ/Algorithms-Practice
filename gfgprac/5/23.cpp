c7b2a9fb826a524cf3eeb0c65e84d5908bdb85f5
AadityaJ <aadityajamuar@gmail.com>
2017-06-10 22:39:27 +0530

    adding

--- gfgprac/5/23.cpp
+++ gfgprac/5/23.cpp
@@ -2,12 +2,23 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <climits>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		int mx=0;
+		for(int i=n-1;i>=0;i--){
+			for(int j=0;j<i;j++){
+				if(arr[i]>=arr[j]){mx=max(i-j,mx);}
+			}
+		}
+		cout<<mx<<endl;
 	}
 	return 0;
 }
