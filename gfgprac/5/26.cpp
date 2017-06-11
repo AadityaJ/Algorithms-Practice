3d5360c8007e589a75669a779a5fe4d1744f617d
AadityaJ <aadityajamuar@gmail.com>
2017-06-11 15:24:52 +0530

    adding

--- gfgprac/5/26.cpp
+++ gfgprac/5/26.cpp
@@ -2,12 +2,44 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define NC 256
 using namespace std;
-int main(int argc, char const *argv[]){
-	int t;
-	cin>>t;
-	while(t--){
+void findLargestUniqueSubstring(char input[], char output[]){
+	// Write your code here
+    int curlen=1,mxlen=1,index=0,n=strlen(input),i;
+    int visited[NC];
+    for(i=0;i<NC;i++) visited[i]=-1;
+    visited[input[0]]=0;
+    for(i=1;i<n;i++){
+        int prev_index=visited[input[i]];
 
+        if(prev_index==-1 || i-curlen>prev_index) curlen++;
+        else{
+            if(curlen>mxlen){
+                mxlen=curlen;
+                index=i;
+            }
+            curlen=i-prev_index;
+        }
+        visited[input[i]]=i;
+    }
+    if(curlen>mxlen){
+        mxlen=curlen;
+        index=i;
+    }
+    //cout<<mxlen<<" "<<index;
+	int itr=0;
+	for(int x=(index-mxlen);x<index;x++){
+		output[itr++]=input[x];
 	}
+	output[itr]='\0';
+}
+int main(int argc, char const *argv[]){
+	int t=1;
+	//cin>>t;
+		char in[]="abcdefgh";
+		char out[]="";
+		findLargestUniqueSubstring(in,out);
+		cout<<out<<endl;
 	return 0;
 }
