79fc7638890f049fbbcfe1398cc985e504ff24d6
AadityaJ <aadityajamuar@gmail.com>
2017-06-11 12:43:15 +0530

    adding

--- gfgprac/5/25.cpp
+++ gfgprac/5/25.cpp
@@ -2,12 +2,40 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
 using namespace std;
+void reverseStringWordWise(char input[]) {
+    // Write your code here
+    int n = strlen(input);
+    string str="";
+    string ans="";
+    stack<string> st;
+    for(int i=0;i<n;i++){
+        if(input[i]==' '|| i==n-1){
+			if(i==n-1){str.push_back(input[i]);}
+            st.push(str);
+            str="";
+        }
+        else{
+            str.push_back(input[i]);
+        }
+    }
+    while(!st.empty()){
+        ans+=st.top();
+        ans+=" ";
+        st.pop();
+    }
+    for(int i=0;i<(ans.length()-1);i++){
+        input[i]=ans[i];
+    }
+}
 int main(int argc, char const *argv[]){
-	int t;
-	cin>>t;
+	int t=1;
+	//cin>>t;
 	while(t--){
-
+		char c[]="hello world";
+		reverseStringWordWise(c);
+		cout<<c<<endl;
 	}
 	return 0;
 }
