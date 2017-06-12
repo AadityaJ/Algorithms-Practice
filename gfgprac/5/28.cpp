3202cca699a9aa4211150d65e193b055080451e5
AadityaJ <aadityajamuar@gmail.com>
2017-06-12 10:49:01 +0530

    adding

--- gfgprac/5/28.cpp
+++ gfgprac/5/28.cpp
@@ -2,12 +2,28 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define MX 10001
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n,x;
+		cin>>n;
+		int arr[n];
+		int mp[MX],nt[MX];
+		for(int i=0;i<MX;i++) {mp[i]=0;nt[i]=0;}
+		for(int i=0;i<n;i++) {
+			cin>>arr[i];
+		}
+		cin>>x;
+		int count=0;
+		for(int i=0;i<n;i++){
+			if(mp[arr[i]] && !nt[arr[i]]) {count+=mp[arr[i]];nt[arr[i]]=1;}
+			mp[x+arr[i]]++;
+			if(arr[i]-x>0 && x!=0)mp[arr[i]-x]++;
+		}
+		cout<<count<<endl;
 	}
 	return 0;
 }
