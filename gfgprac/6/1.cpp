1a08985532a998ed1c25d915636990fde7010834
AadityaJ <aadityajamuar@gmail.com>
2017-06-13 12:12:04 +0530

    adding

--- gfgprac/6/1.cpp
+++ gfgprac/6/1.cpp
@@ -11,19 +11,15 @@ int main(int argc, char const *argv[]){
 		cin>>n;
 		int arr[n];
 		for(int i=0;i<n;i++) cin>>arr[i];
-		int o_i=n,e_i=-1;
-		int i=0,j=n-1;
-		while(i<j){
-			while(arr[i]%2==0) i++;
-			while(arr[j]%2==1) j--;
-			if(i>=j) break;
-			int temp=arr[i];
-			arr[i]=arr[j];
-			arr[j]=temp;
-			i++;
-			j--;
+		sort(arr,arr+n);
+		std::vector<int> o;
+		std::vector<int> e;
+		for(int i=0;i<n;i++){
+			if(arr[i]%2==0) cout<<arr[i]<<" ";
 		}
-		for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+		for(int i=0;i<n;i++){
+			if(arr[i]%2==1) cout<<arr[i]<<" ";
+		}
 		cout<<endl;
 	}
 	return 0;
