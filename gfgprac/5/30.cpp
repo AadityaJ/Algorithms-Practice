781e4b88725ae91ba38795339b9181a086665a6c
AadityaJ <aadityajamuar@gmail.com>
2017-06-13 11:26:15 +0530

    adding

--- gfgprac/5/30.cpp
+++ gfgprac/5/30.cpp
@@ -7,7 +7,24 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		int mx_so=0,count=0;
+		for (int i = 0; i < n; i++) {
+			/* code */
+			if(arr[mx_so]==arr[i]) count++;
+			else count--;
+			if(!count){
+				count++;
+				mx_so=i;
+			}
+		}
+		count =0;
+		for(int i=0;i<n;i++) if(arr[mx_so]==arr[i]) count++;
+		if(count>(n/2)) cout<<arr[mx_so]<<endl;
+		else std::cout << "NO Majority Element" << std::endl;
 	}
 	return 0;
 }
