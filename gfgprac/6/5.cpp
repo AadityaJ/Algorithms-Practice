188b1fd09712d16cd4fca4d2cf4d8a3824fd2433
AadityaJ <aadityajamuar@gmail.com>
2017-06-15 21:09:37 +0530

    adding

--- gfgprac/6/5.cpp
+++ gfgprac/6/5.cpp
@@ -2,12 +2,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <climits>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int min1=INT_MAX,min2=INT_MAX,x=0;
+		for(int i=0;i<n;i++){
+			cin>>x;
+			if(x<min1){
+				min2=min1;
+				min1=x;
+			}
+			else if(x<min2 && x!=min1){
+				min2=x;
+			}
+		}
+		if(min1==INT_MAX || min2==INT_MAX) cout<<"-1"<<endl;
+		else cout<<min1<<" "<<min2<<endl;
 	}
 	return 0;
 }
