6359bdd9696a8c8907b4ea3f81e80310e2ffe4e4
AadityaJ <aadityajamuar@gmail.com>
2017-06-16 23:27:03 +0530

    adding

--- gfgprac/6/9.cpp
+++ gfgprac/6/9.cpp
@@ -1,13 +1,23 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+bool isSat(int n){
+	while(n){
+		if((n%10>5) ||(n%10==0)) return 0;
+		n/=10;
+	}
+	return 1;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int s=0;
+		for(int i=1;i<=n;i++){
+			if(isSat(i)) s++;
+		}
+		cout<<s<<endl;
 	}
 	return 0;
 }
