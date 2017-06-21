5e88646ff1589d78c59a362c77a251ab2af4d5ec
AadityaJ <aadityajamuar@gmail.com>
2017-06-21 11:43:38 +0530

    adding

--- gfgprac/6/16.cpp
+++ gfgprac/6/16.cpp
@@ -2,12 +2,29 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
+#define MX 10001
 using namespace std;
+int dp[MX][MX];
+int f(int a,int b,int sum,int i){
+	if(sum>=a) return sum==a;
+	if(i>a) return 0;
+	if(dp[sum][i]!=-1) return dp[sum][i];
+	return dp[sum][i]=f(a,b,sum+pow(i,b),i+1)+f(a,b,sum,i+1);
+}
+int allWays(int a, int b) {
+	for(int i=0;i<a;i++){
+		for(int j=0;j<a;j++) dp[i][j]=-1;
+	}
+	return f(a,b,0,1);
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int a,b;
+		cin>>a>>b;
+		cout<<allWays(a,b)<<endl;
 	}
 	return 0;
 }
