7fc8a81ecc966ddeb425b234db55cfe917dfe7e8
AadityaJ <aadityajamuar@gmail.com>
2017-06-21 11:15:47 +0530

    adding

--- gfgprac/6/15.cpp
+++ gfgprac/6/15.cpp
@@ -3,11 +3,22 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+void f(string str,int o,int c,int i,int n){
+	if(i==n){
+		if(o==c) cout<<str<<endl;
+		return ;
+	}
+	if(o<c) return;
+	f(str+"(",o+1,c,i+1,n);
+	f(str+")",o,c+1,i+1,n);
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		f("",0,0,0,(2*n));
 	}
 	return 0;
 }
