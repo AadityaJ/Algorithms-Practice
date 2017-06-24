fd3ff90e3b6ae7cc8b1a0796166c5c0de24ccf4a
Aaditya Jamuar <aaditya@ABCs-MacBook-Pro.local>
2017-06-24 11:43:54 +0530

    adding

--- gfgprac/6/18.cpp
+++ gfgprac/6/18.cpp
@@ -2,12 +2,33 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
 using namespace std;
+int f(string str){
+	stack<int>s;
+	s.push(-1);
+	int mx=0;
+	for(int i=0;i<str.length();i++){
+		if(str[i]=='(') s.push(i);
+		else{
+			s.pop();
+			if(s.empty()){
+				s.push(i);
+			}
+			else{
+				mx=max(mx,i-s.top());
+			}
+		}
+	}
+	return mx;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str;
+		cin>>str;
+		cout<<f(str)<<endl;
 	}
 	return 0;
 }
