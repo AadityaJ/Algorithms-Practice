f2bf919a2d318de4abd92ae5e7e1313a633581fa
AadityaJ <aadityajamuar@gmail.com>
2017-06-26 11:09:24 +0530

    adding

--- gfgprac/6/21.cpp
+++ gfgprac/6/21.cpp
@@ -3,11 +3,19 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+struct normies{  //GET OUT REEEEEEEEEEE
+	int pet;
+	int dist;
+};
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		normies val[n];
+		for(int i=0;i<n;i++){
+			cin>>val[i].pet>>
+		}
 	}
 	return 0;
 }
