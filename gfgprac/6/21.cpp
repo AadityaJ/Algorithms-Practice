71d6b8c7495a5a8aedf7d86bb6f1504d26c17a68
AadityaJ <aadityajamuar@gmail.com>
2017-06-26 15:16:12 +0530

    adding

--- gfgprac/6/21.cpp
+++ gfgprac/6/21.cpp
@@ -2,20 +2,32 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
 using namespace std;
-struct normies{  //GET OUT REEEEEEEEEEE
-	int pet;
-	int dist;
-};
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
 		int n;
-		normies val[n];
-		for(int i=0;i<n;i++){
-			cin>>val[i].pet>>
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++) cin>>a[i];
+		int ans[n];
+		stack<int> s;
+		s.push(0);
+		for(int i=1;i<n;i++){
+			while(!s.empty()&&a[s.top()]<a[i]){
+				ans[s.top()]=a[i];
+				s.pop();
+			}
+			s.push(i);
 		}
+		while(!s.empty()){
+			ans[s.top()]=-1;
+			s.pop();
+		}
+		for(int i=0;i<n;i++) cout<<ans[i]<<" ";
+		cout<<endl;
 	}
 	return 0;
 }
