b342608ebc067214fb1f983bb0c86388d35038a5
AadityaJ <aadityajamuar@gmail.com>
2017-06-27 20:41:45 +0530

    adding

--- DS/linkedlist/new_sll.cpp
+++ DS/linkedlist/new_sll.cpp
@@ -21,41 +21,41 @@ public:
         Node *temp=new Node(x,NULL);
         if(!head && !tail){
             head=temp;tail=temp;
-            delete temp;
             return ;
         }
         tail->next=temp;
         tail=temp;
-        delete temp;
         return ;
     }
     void push_front(int x){
         Node *temp=new Node(x,head);
         if(!head && !tail){
             head=temp;tail=temp;
-            delete temp;
             return ;
         }
         head=temp;
-        delete temp;
         return ;
-    }
+    };
     void print(){
         Node *curr=head;
         while(curr){
             cout<<curr->data<<" ";
             curr=curr->next;
         }
+        cout<<endl;
         return ;
     }
 };
-int main(int argc, char const *argv[]) {
+void test1(){
     SLL obj;
     obj.push_back(1);
     obj.push_back(2);
-    obj.print();
     obj.push_back(3);
     obj.push_front(10);
-    //obj.print();
+    obj.push_front(20);
+    obj.print();
+}
+int main(int argc, char const *argv[]) {
+    test1();
     return 0;
 }
