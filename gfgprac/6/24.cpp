9688e49ac3d66b220ee2c1a22ff9879091f2ba75
AadityaJ <aadityajamuar@gmail.com>
2017-07-01 21:24:34 +0530

    adding

--- gfgprac/6/24.cpp
+++ gfgprac/6/24.cpp
@@ -3,11 +3,29 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+void swap(int &a,int &b){
+	int temp=a;
+	a=b;
+	b=temp;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n];
+		for (int i = 0; i < n; i++) {
+			cin>>arr[i];
+		}
+		sort(arr,arr+n);
+		for(int i=1;i<(n-1);i+=2){
+			swap(arr[i],arr[i+1]);
+		}
+		for (int i = 0; i < n; i++) {
+			cout<<arr[i]<<' ';
+		}
+		std::cout << '\n';
 	}
 	return 0;
 }
