39180256f07a1272395c0f2120e5bbdc0595067e
AadityaJ <aadityajamuar@gmail.com>
2017-07-03 21:43:31 +0530

    adding

--- gfgprac/6/25.cpp
+++ gfgprac/6/25.cpp
@@ -3,11 +3,36 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int find(std::vector<char> v,char c){
+	int index=-1;
+	for(int i=0;i<v.size();i++){
+		if(v[i]==c){index=i;break;}
+	}
+	return index;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
+		int n;
+		cin>>n;
+		std::vector<char> v;
+		string ans="-1";
+		int mp[256]={0};
+		char c;
+		for(int i=0;i<n;i++){
+			cin>>c;
+			mp[c]++;
+			if(i==0){ans=c;cout<<ans<<" ";mp[c]++;continue;}
+			if(mp[c]==1) v.push_back(c);
 
+			if(mp[c]!=1){
+				
+			}
+			cout<<ans<<' ';
+
+		}
+		cout<<endl;
 	}
 	return 0;
 }
