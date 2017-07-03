0fdb09df370f3ce9cd9e592b1cf7399e318cb389
AadityaJ <aadityajamuar@gmail.com>
2017-07-03 22:19:20 +0530

    adding

--- gfgprac/6/25.cpp
+++ gfgprac/6/25.cpp
@@ -2,37 +2,32 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <map>
+#include <queue>
 using namespace std;
-void rinse(std::vector<char>&v,int mp[]){
-	for(int i=0;i<v.size();i++){
-		if(mp[v[i]]>1) v.erase(v.begin()+i);
-	}
-}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
 		int n;
 		cin>>n;
-		std::vector<char> v;
+		queue<char>q;
 		char ans='0';
 		int mp[256]={0};
 		char c;
 		for(int i=0;i<n;i++){
 			cin>>c;
 			mp[c]++;
-			if(mp[c]==1) v.push_back(c);
-			rinse(v,mp);
-			if(ans=='0' && v.size()==0){cout<<"-1 ";continue;}
-			if(ans=='0') ans = v.front();
-			else if(mp[ans]>1){
-				v.erase(v.begin());
-				if(v.size()==0) ans='0';
-				else ans=v.front();
+			q.push(c);
+			bool is=1;
+			while(!q.empty()){
+				if(mp[q.front()]==1){
+					cout<<q.front()<<' ';
+					is=0;
+					break;
+				}
+				q.pop();
 			}
-			if(ans=='0') cout<<"-1"<<" ";
-			else cout<<ans<<" ";
+			if(is) cout<<"-1 ";
 		}
 		cout<<endl;
 	}
