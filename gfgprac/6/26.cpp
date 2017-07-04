9333497a75af9cef2211e8176792a624068a236b
AadityaJ <aadityajamuar@gmail.com>
2017-07-04 15:25:00 +0530

    adding

--- gfgprac/6/26.cpp
+++ gfgprac/6/26.cpp
@@ -7,7 +7,18 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n,x;
+		std::vector<int> odd;
+		std::vector<int> even;
+		cin>>n;
+		for(int i=0;i<n;i++){
+			cin>>x;
+			if(x%2) odd.push_back(x);
+			else even.push_back(x);
+		}
+		for(int i=0;i<even.size();i++) cout<<even[i]<<" ";
+		for(int i=0;i<odd.size();i++) cout<<odd[i]<<" ";
+		cout<<endl;
 	}
 	return 0;
 }
