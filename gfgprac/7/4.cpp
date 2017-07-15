448b80b80793d58b4ca0337cc9a8511e5a7bd402
AadityaJ <aadityajamuar@gmail.com>
2017-07-16 03:12:05 +0530

    adding

--- gfgprac/7/4.cpp
+++ gfgprac/7/4.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int lis(int *arr,int i,int n,int lst,int **dp){
 	if(i==n) return 0;
 	if(dp[i][lst]!=-1) return dp[i][lst];
-	if(arr[i]<lst) return dp[i][lst]=lis(arr,i+1,n,lst,dp);
+	if(arr[i]<=lst &&lst!=0) return dp[i][lst]=lis(arr,i+1,n,lst,dp);
 	return dp[i][lst]=max(lis(arr,i+1,n,lst,dp),1+lis(arr,i+1,n,arr[i],dp));
 }
 int main(int argc, char const *argv[]){
