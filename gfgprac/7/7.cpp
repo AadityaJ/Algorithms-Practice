2304a2bf823c4f44091a6ee4835f6df1b6de5bd4
AadityaJ <aadityajamuar@gmail.com>
2017-07-16 14:58:47 +0530

    adding

--- gfgprac/7/7.cpp
+++ gfgprac/7/7.cpp
@@ -5,11 +5,45 @@
 #include <vector>
 #include <climits>
 using namespace std;
+void trav(int &cnt,bool g[][51],int i,int j,int n,int m,bool visited[][51]){
+	if(!g[i][j]) return ;
+	visited[i][j]=1;
+	cnt++;
+	if(i>0) trav(cnt,g,i-1,j,n,m,visited);
+	//if(i>0) trav(cnt,g,i-1,j,n,m,visited);
+	if(i<n-1) trav(cnt,g,i+1,j,n,m,visited);
+	//if(i<n-1) trav(cnt,g,i+1,j,n,m,visited);
+	if(j>0) trav(cnt,g,i,j-1,n,m,visited);
+	if(j<n-1) trav(cnt,g,i,j+1,n,m,visited);
+}
+int f(bool g[][51],int n,int m,bool visited[][51]){
+	int mx=0;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(g[i][j]&&!visited[i][j]){
+				int cnt=0;
+				trav(cnt,g,i,j,n,m,visited);
+				mx=max(mx,cnt);
+			}
+		}
+	}
+	return mx;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n,m;
+		cin>>n>>m;
+		bool g[n][m];
+		bool visited[n][m];
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				cin>>g[i][j];
+				visited[i][j]=0;
+			}
+		}
+		cout<<f(g,n,m,visited);
 	}
 	return 0;
 }
