7245ba4db8ee2696a5ff64af43546ec5914893f1
AadityaJ <aadityajamuar@gmail.com>
2017-07-17 13:23:06 +0530

    adding

--- gfgprac/7/8.cpp
+++ gfgprac/7/8.cpp
@@ -3,13 +3,18 @@
 #include <vector>
 #include <algorithm>
 #include <vector>
-#include <climits>
+#include <cmath>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
+		long long int n;
+		cin>>n;
+		//unsigned int a=INT_MAX;
+		long long int x=pow(2,31);
 
+		cout<<(x^n)<<endl;
 	}
 	return 0;
 }
