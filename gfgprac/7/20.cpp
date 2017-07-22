3fa25546860e9707d42e0e64ca229d9906f4c255
AadityaJ <aadityajamuar@gmail.com>
2017-07-22 11:48:24 +0530

    adding

--- gfgprac/7/20.cpp
+++ gfgprac/7/20.cpp
@@ -3,13 +3,17 @@
 #include <vector>
 #include <algorithm>
 #include <vector>
-#include <climits>
 using namespace std;
+bool f(string str){
+ 	return ((stoll(str,0,2))%3==0);
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		string str;
+		cin>>str;
+		cout<<f(str)<<endl;
 	}
 	return 0;
 }
