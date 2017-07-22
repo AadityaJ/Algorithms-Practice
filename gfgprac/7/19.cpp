19dc8b7cece33a90148d9a735e6ac5ff6ff68c9b
AadityaJ <aadityajamuar@gmail.com>
2017-07-22 10:29:23 +0530

    adding

--- gfgprac/7/19.cpp
+++ gfgprac/7/19.cpp
@@ -4,6 +4,20 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+int binS(int *arr,int n,int x){
+	int hi=n-1;
+	int lo=0;
+	while(hi>=lo){
+		int mid=lo+((hi-lo)/2);
+		if(arr[mid]==x){return mid;}
+		else if(arr[mid]>x){
+			hi=mid-1;
+		}else{
+			lo=mid+1;
+		}
+	}
+	return -1;
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
@@ -11,12 +25,13 @@ int main(int argc, char const *argv[]){
 		int n;
 		cin>>n;
 		int arr[n];
-		for(int i=0;i<n;i++) cin>>arr[i];
+		int s_arr[n];
+		for(int i=0;i<n;i++) {cin>>arr[i];s_arr[i]=arr[i];}
+		sort(s_arr,s_arr+n);
 		int cnt=0;
 		for(int i=0;i<n;i++){
-			for(int j=i+1;j<n;j++){
-				if(arr[i]>arr[j]) cnt++;
-			}
+			int x=binS(s_arr,n,arr[i]);
+			if(x>i) cnt+=(x-i);
 		}
 		cout<<cnt<<endl;
 	}
