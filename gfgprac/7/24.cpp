77c632abcf73464a6950afa61be73f976ec8288d
AadityaJ <aadityajamuar@gmail.com>
2017-07-23 23:35:21 +0530

    adding

--- gfgprac/7/24.cpp
+++ gfgprac/7/24.cpp
@@ -3,12 +3,25 @@
 #include <vector>
 #include <algorithm>
 #include <vector>
-#include <climits>
 using namespace std;
+pair<bool,string> changetoNg(string str){
+	bool is=0;
+	string ax="";
+	for(int i=0;i<str.length()-1;i++){
+		if(str[i]=='n' && str[i+1]=='g'){
+			is=1;
+			ax+=
+		}
+	}
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
+		string str1,str2;
+		cin>>str1>>str2;
+		bool is_ng_1=changetoNg(str1);
+		bool is_ng_2=changetoNg(str2);
 
 	}
 	return 0;
