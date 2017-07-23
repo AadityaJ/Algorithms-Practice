edd5f44b20fbf56310ef0f23beb9c13d1f9e60d7
AadityaJ <aadityajamuar@gmail.com>
2017-07-23 20:15:30 +0530

    adding

--- gfgprac/7/23.cpp
+++ gfgprac/7/23.cpp
@@ -3,13 +3,22 @@
 #include <vector>
 #include <algorithm>
 #include <vector>
-#include <climits>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++) cin>>arr[i];
+		bool x=next_permutation(arr,arr+n);
+		if(x){
+			for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+		}else{
+			for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+		}
+		cout<<endl;
 	}
 	return 0;
 }
