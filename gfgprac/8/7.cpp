cc47101013e4228117bfe42dbbef445665cd6c22
AadityaJ <aadityajamuar@gmail.com>
2017-07-26 14:36:40 +0530

    adding

--- gfgprac/8/7.cpp
+++ gfgprac/8/7.cpp
@@ -8,7 +8,11 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		if(!(n&(n+1))) cout<<"YES"<<endl;
+		else cout<<"NO\n";
+		//cout<<(!n==0)<<endl;
 	}
 	return 0;
 }
