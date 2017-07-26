739a3469476c49792305758f283331bf68a1edc4
AadityaJ <aadityajamuar@gmail.com>
2017-07-26 11:16:47 +0530

    adding

--- gfgprac/8/4.cpp
+++ gfgprac/8/4.cpp
@@ -4,11 +4,22 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+int f(int *arr,int i,int n,int *mem){
+	if(i>=n) return 0;
+	if(mem[i]!=-1) return mem[i];
+	return mem[i]=max(f(arr,i+1,n,mem),arr[i]+f(arr,i+2,n,mem));
+}
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int arr[n];
+		int *mem=new int[n+1];
+		for(int i=0;i<n;i++) mem[i]=-1;
+		for(int i=0;i<n;i++) cin>>arr[i];
+		cout<<f(arr,0,n,mem)<<endl;
 	}
 	return 0;
 }
--- gfgprac/8/5.cpp
+++ gfgprac/8/5.cpp
@@ -3,12 +3,26 @@
 #include <vector>
 #include <algorithm>
 #include <vector>
+#include <map>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		string str;
+		map<string,int> mp;
+		for(int i=0;i<n;i++){
+			cin>>str;
+			mp[str]++;
+		}
+		int ans=0;
+		map<string,int>:: iterator it;
+		for(it=mp.begin();it!=mp.end();it++){
+			if(it->second==2) ans++;
+		}
+		cout<<ans<<endl;
 	}
 	return 0;
 }
