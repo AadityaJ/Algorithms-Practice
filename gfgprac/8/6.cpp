9de3f9ce063a8a0bef49dc5362a76e581c2f1902
AadityaJ <aadityajamuar@gmail.com>
2017-07-26 11:30:07 +0530

    adding

--- gfgprac/8/6.cpp
+++ gfgprac/8/6.cpp
@@ -3,12 +3,32 @@
 #include <vector>
 #include <algorithm>
 #include <vector>
+#include <map>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		map<string,int> mp;
+		string str;
+		int n;
+		cin>>n;
+		for(int i=0;i<n;i++){
+			cin>>str;
+			mp[str]++;
+		}
+		int mx=0;
+		map<string,int>:: iterator it;
+		for(it=mp.begin();it!=mp.end();it++){
+			if(it->second>mx){mx=it->second;str=it->first;}
+		}
+		mp[str]=0;
+		mx=0;
+		for(it=mp.begin();it!=mp.end();it++){
+			//cout<<it->first<<" "<<it->second<<endl;
+			if(it->second>mx){mx=it->second;str=it->first;}
+		}
+		cout<<str<<endl;
 	}
 	return 0;
 }
