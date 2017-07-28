f223fba6e7fd7fa9ccff479256611aa419f16b55
AadityaJ <aadityajamuar@gmail.com>
2017-07-29 00:58:13 +0530

    adding

--- gfgprac/8/8.cpp
+++ gfgprac/8/8.cpp
@@ -3,12 +3,27 @@
 #include <vector>
 #include <algorithm>
 #include <vector>
+#include <map>
+#include <queue>
 using namespace std;
 int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int c=1;
+		queue<int>q;
+		q.push(1);
+		while(c<=n){
+			int x=q.front();
+			q.push(x*2);
+			q.push(x*3);
+			q.push(x*5);
+			q.pop();
+			c++;
+		}
+		cout<<q.front()<<endl;
 	}
 	return 0;
 }
