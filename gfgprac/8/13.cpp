45ee36866afde914fd8cf72ca0266ee04e0785c8
AadityaJ <aadityajamuar@gmail.com>
2018-01-10 20:21:24 +0530

    adding

--- gfgprac/8/13.cpp
+++ gfgprac/8/13.cpp
@@ -3,12 +3,16 @@
 #include <vector>
 #include <algorithm>
 #include <vector>
+#include <map>
 using namespace std;
 int main(int argc, char const *argv[]){
-	int t;
-	cin>>t;
-	while(t--){
-
-	}
+	int k,l,m,n,d;
+	cin>>k>>l>>m>>n>>d;
+	map<int,bool> mp;
+	for(int i=k;i<=d;i+=k) mp[i]=1;
+	for(int i=l;i<=d;i+=l) mp[i]=1;
+	for(int i=m;i<=d;i+=m) mp[i]=1;
+	for(int i=n;i<=d;i+=n) mp[i]=1;
+	cout<<mp.size()<<endl;
 	return 0;
 }
